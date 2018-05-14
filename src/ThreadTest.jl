d4a277092ea51cdbdbfa4fa2d8d1ebd7188e48b1
florian oswald <florian.oswald@gmail.com>
2018-05-14 22:07:07 +0200

    update

--- src/ThreadTest.jl
+++ src/ThreadTest.jl
@@ -2,28 +2,56 @@ module ThreadTest
 
 	mutable struct Model 
 		a::Vector
+		r::Vector{Float64}
 		id::Vector{Int}
 	end
 
 	mutable struct LModel
-		b::Vector
+		b::Array
+		function LModel(i::Int)
+			new(ones(10,10))
+		end
 	end
 
-	function doit(n=10)
-		m = Model(zeros(n),zeros(Int,n))
+	function doit0(n=10)
+		m = Model(zeros(n),zeros(Int,n),zeros(n))
 		Threads.@threads for i in 1:n
 			dothread!(m,i)
 		end
 		return m
 	end
 
-	function dothread!(m::Model,i::Int)
-		Lm = LModel(rand(1_000))  # create a local model
+	function doit1(n=10)
+		m = Model(zeros(n),zeros(Int,n),rand(n))
+		x = 1.3
+		Threads.@threads for i in 1:n
+			Lm = LModel(i)  # create a local model
+			lx = x + i
+			id = Threads.threadid()  # note which id did which slot
+			m.id[i] = id  # note which id did which slot
+			m.a[i] = mean(Lm.b .* id) 
+		end
+		return m,hcat(m.a,m.id)
+	end
+	function doit2()
+		a = zeros(Int,10)
+		y = zeros(10)
+		x = 1.3
+		Threads.@threads for i = 1:10
+			lx = x + i
+            a[i] = Threads.threadid()
+            y[i] = lx
+   		end
+   		return (a,y)
+	end
 
+	function dothread!(m::Model,i::Int)
+		Lm = LModel(i)  # create a local model
 		# do some work on the local model and store the results in 
 		# the global model array
-		m.a[i] = median(Lm.b)
 		m.id[i] = Threads.threadid()  # note which id did which slot
+		m.a[i] = mean(Lm.b .* m.id[i]) 
+		m.r[i] = rand()
 	end
 
 end # module
